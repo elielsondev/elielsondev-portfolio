3a4de4322d4be6eef8c453bf21a79c30025d7b7a
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-10-23 12:51:44 -0300

    feat - footer implementation

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+import './css/Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Designed and developed by Elielson Nascimento.</p>
+      <p>© 2024. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer;
